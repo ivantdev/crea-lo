fe86c4b070501f93b5d1929d25c9cdacf103cf5e
Sebastian Castañeda <sebastiansnipez@gmail.com>
2022-12-18 13:38:40 -0500

    Testing collection

--- config/server.js
+++ config/server.js
@@ -1,13 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 1338),
   app: {
     keys: env.array("APP_KEYS"),
   },
-  url: env("PUBLIC_URL", "https://strapi-production-faee.up.railway.app"),
-  cron: { enabled: true },
-  cors: { enabled: true, origin: ['*'] },
-  admin: {
-    auth: { secret: env("ADMIN_JWT_SECRET", "9bf8cc74ab83590b280df0851beaec60") }
-  }
 });
