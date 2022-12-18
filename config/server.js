9d646c0b965f3c0c6026eab566cd1b5e0c2ad8d4
Sebastian Castañeda <sebastiansnipez@gmail.com>
2022-12-18 15:14:15 -0500

    change server settings

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
+  port: env.int('PORT', 1339),
+  url: env('URL', 'http://localhost:1339'),
   app: {
     keys: env.array("APP_KEYS"),
   },
