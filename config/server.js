5a609a2cdcb7457723a23796524dece1b8ab95a0
Sebastián Castañeda <69208353+Sebastian-na@users.noreply.github.com>
2022-12-18 10:12:48 -0500

    Update server.js

--- config/server.js
+++ config/server.js
@@ -4,5 +4,10 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
-  url: env("PUBLIC_URL", "https://strapi-production-faee.up.railway.app")
+  url: env("PUBLIC_URL", "https://strapi-production-faee.up.railway.app"),
+  cron: { enabled: true },
+  cors: { enabled: true, origin: ['*'] },
+  admin: {
+    auth: { secret: env("ADMIN_JWT_SECRET", "9bf8cc74ab83590b280df0851beaec60") }
+  }
 });
